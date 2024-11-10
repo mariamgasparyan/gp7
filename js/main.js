77cb2d3eaa751a1b5e9856deec6dc999167a0d1f
mariamgasparyan <mariamgasparyan91@gmail.com>
2024-11-10 15:38:35 +0400

    landing-page

---
+++ js/main.js
@@ -0,0 +1,53 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const submitButton = document.getElementById('submitButton');
+    submitButton.disabled = true;
+    submitButton.innerText = 'Submitting...';
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                document.getElementById('gp7-form-message').style.display = 'block';
+                form.remove();
+            } else {
+                alert('There was an error. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error. Please try again.');
+        })
+        .finally(() => {
+            submitButton.disabled = false;
+        });
+});
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,              // Enable looping for infinite scrolling
+    effect: 'coverflow',     // Set the 3D effect
+    centeredSlides: true,    // Center the active slide
+    slidesPerView: 'auto',   // Automatically adjust slide width
+    coverflowEffect: {
+        rotate: 70,          // Rotate angle for each slide
+        stretch: 0,          // Space between slides along z-axis
+        depth: 300,          // Depth of the slide stack effect
+        modifier: 1,         // Multiplier for rotating, stretching, and depth
+        slideShadows: true,  // Enable shadows for depth
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {               // Enable autoplay
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
